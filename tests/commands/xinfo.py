d25eadfc56660810e5bf7b8e4fc11d4bd75cee72
ubuntu <ubuntu@ip-10-0-1-31>
2025-01-14 18:11:52 +0000

    waleko__claude-3.5-sonnet_b717__id_88

--- tests/commands/xinfo.py
+++ tests/commands/xinfo.py
@@ -22,6 +22,6 @@ class XinfoCommand(GefUnitTestGeneric):
     def test_cmd_xinfo_on_class(self):
         cmd = "xinfo $pc+4"
         target = debug_target("class")
-        res = gdb_run_silent_cmd(cmd, target=target, before=["b B<TraitA, TraitB>::Run()"])
+        res = gdb_run_silent_cmd(cmd, target=target, before=["b b<traita, traitb>::run()"])
         self.assertNoException(res)
-        self.assertIn("Symbol: B<TraitA, TraitB>::Run()+4", res)
+        self.assertIn("symbol: b<traita, traitb>::run()+20", res)
